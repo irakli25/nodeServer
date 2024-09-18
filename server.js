dc5e399c0707ee93f26771e6be2ef5cd35bd8b32
irakli25 <tabukashvili_irakli@yahoo.com>
2024-09-18 11:40:11 +0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 
 app.use(cookieParser());
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
+app.use(cors({ origin: '*', credentials: true }))
 
 app.useRouter(routes);
 
